2f63b9c5adf0407c51e7d2f144f2e4243317f421
Sherub Thakur <sherub.thakur@gmail.com>
2023-03-17 19:21:37 +0530

    Reorganize code

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,124 @@
+use reqwest::blocking::{Client, RequestBuilder};
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use structopt::StructOpt;
 
 const OPENAI_API_URL: &str = "https://api.openai.com/v1/chat/completions";
-
-#[derive(Serialize)]
-struct GPTRequest<'a> {
-    model: &'a str,
-    messages: &'a [Chat<'a>],
-}
-
-#[derive(Serialize)]
-struct Chat<'a> {
-    role: &'a str,
-    content: &'a str,
-}
+const PROMPT_PREFIX: &str = "Assume you are a Linux/Unix expert. \
+    Be concise in your response. \
+    Which command ";
 
 #[derive(Debug, StructOpt)]
 #[structopt(
     name = "file",
     about = "A command line tool to help you find the right command for the job"
 )]
-struct Opt {
-    #[structopt(help = "The operation you want to perform")]
-    operation: String,
+struct CliArgs {
+    #[structopt(help = "Description of the command you want to find")]
+    command_description: String,
 }
 
-fn get_response(operation: &str) -> anyhow::Result<String> {
-    let prompt = format!("What is a command that {}?", operation);
-    let message = Chat {
-        role: "user",
-        content: &prompt,
-    };
-    let request_body = GPTRequest {
-        model: "gpt-3.5-turbo",
-        messages: &[message],
-    };
-    let client = reqwest::blocking::Client::new();
-    let request = client
-        .post(OPENAI_API_URL)
-        .header("Content-Type", "application/json")
-        .header("Authorization", &format!("Bearer {}", get_api_key()))
-        .json(&request_body);
-    let response = request.send()?;
-    let response_body: Value = response.json()?;
-    let response_content = response_body["choices"][0]["message"]["content"]
-        .as_str()
-        .unwrap()
-        .to_string();
+#[derive(Debug, Serialize, Deserialize)]
+struct ChatFormatMessage {
+    role: String,
+    content: String,
+}
 
-    Ok(response_content)
+#[derive(Serialize)]
+struct GPTRequest<'a> {
+    model: &'a str,
+    messages: &'a [&'a ChatFormatMessage],
+}
+
+#[derive(Debug, Deserialize)]
+struct GPTResponse {
+    choices: Vec<Choice>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Choice {
+    message: ChatFormatMessage,
+}
+
+struct OpenAIWrapper<'a> {
+    model: &'a str,
+    api_endpoint: &'a str,
+    api_key: &'a str,
+    prompt_prefix: &'a str,
+    client: &'a Client,
+}
+
+impl<'a> OpenAIWrapper<'a> {
+    fn new(api_key: &'a str, client: &'a Client) -> Self {
+        OpenAIWrapper {
+            model: "gpt-3.5-turbo",
+            api_endpoint: OPENAI_API_URL,
+            api_key: api_key,
+            prompt_prefix: PROMPT_PREFIX,
+            client: &client,
+        }
+    }
+
+    fn make_request(&self, command_description: &str) -> anyhow::Result<RequestBuilder> {
+        let prompt = format!("{} {}?", self.prompt_prefix, command_description);
+        let message = ChatFormatMessage {
+            role: "user".to_string(),
+            content: prompt,
+        };
+        let request_body = GPTRequest {
+            model: &self.model,
+            messages: &[&message],
+        };
+
+        let request = self
+            .client
+            .post(self.api_endpoint)
+            .header("Content-Type", "application/json")
+            .header("Authorization", &format!("Bearer {}", &self.api_key))
+            .json(&request_body);
+
+        Ok(request)
+    }
+
+    fn get_response(&self, operation: &str) -> anyhow::Result<String> {
+        let request = self.make_request(operation)?;
+        let response = request.send()?;
+
+        if response.status() != reqwest::StatusCode::OK {
+            return Err(anyhow::anyhow!(
+                "Response code is not 200 OK: {:?}",
+                response.status()
+            ));
+        }
+
+        let response_body: GPTResponse = response.json()?;
+        let response_content = match response_body.choices.first() {
+            Some(choice) => choice.message.content.to_string(),
+            None => {
+                return Err(anyhow::anyhow!(
+                    "No choices in response body: {:?}",
+                    response_body
+                ))
+            }
+        };
+
+        Ok(response_content)
+    }
 }
 
 fn main() -> anyhow::Result<()> {
-    let opt = Opt::from_args();
+    let opt = CliArgs::from_args();
 
-    let response = get_response(&opt.operation)?;
+    let client = Client::new();
+    let openai_api_key = get_api_key()?;
+    let open_ai_wrapper = OpenAIWrapper::new(&openai_api_key, &client);
+
+    let response = open_ai_wrapper.get_response(&opt.command_description)?;
     println!("{response}");
 
     Ok(())
 }
 
-fn get_api_key() -> String {
-    // TODO: Read from environment variable "OPENAI_API_KEY"
-    std::env::var("OPENAI_API_KEY").unwrap()
+fn get_api_key() -> anyhow::Result<String> {
+    let key = std::env::var("OPENAI_API_KEY")?;
+    Ok(key)
 }
