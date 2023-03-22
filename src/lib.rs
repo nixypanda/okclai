8e191553c92d0763f06825abda7dbc64d3f4c223
Sherub Thakur <sherub.thakur@gmail.com>
2023-03-22 13:02:21 +0530

    Reorganize code: Extract main functionality into a struct

---
+++ src/lib.rs
@@ -0,0 +1,106 @@
+mod openai;
+
+use anyhow::anyhow;
+use futures::{Stream, StreamExt};
+pub use openai::OpenAIWrapper;
+use regex::Regex;
+use std::pin::Pin;
+use std::process::Command;
+
+pub struct Settings {
+    stream: bool,
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        Self { stream: true }
+    }
+}
+
+impl Settings {
+    pub fn new(stream: bool) -> Self {
+        Self { stream }
+    }
+}
+
+pub struct OkClai<'a> {
+    openai: OpenAIWrapper<'a>,
+    settings: Settings,
+}
+
+impl<'a> OkClai<'a> {
+    pub fn new(openai: OpenAIWrapper<'a>, settings: Settings) -> Self {
+        Self { openai, settings }
+    }
+
+    pub async fn execute(&self, command_descripton: &str) -> anyhow::Result<()> {
+        let response: anyhow::Result<String> = if self.settings.stream {
+            let response_stream = Box::pin(
+                self.openai
+                    .get_streaming_response(&command_descripton)
+                    .await?,
+            );
+            let response = self.print_and_extract_response(response_stream).await?;
+            Ok(response)
+        } else {
+            let response = self.openai.get_response(&command_descripton).await?;
+            println!("{}", response);
+            Ok(response)
+        };
+
+        let command = self.extract_code_block(&response?)?;
+        println!("\nCommand to execute: {:?}", command);
+        let result = self.execute_command(&command)?;
+        print!("\nOutput:\n{}", result);
+
+        Ok(())
+    }
+
+    async fn print_and_extract_response(
+        &self,
+        mut stream: Pin<Box<impl Stream<Item = Result<String, anyhow::Error>>>>,
+    ) -> anyhow::Result<String> {
+        let mut response = String::new();
+        while let Some(result_token) = stream.next().await {
+            match result_token {
+                Ok(token) => {
+                    print!("{}", token);
+                    response = format!("{}{}", response, token);
+                }
+                Err(e) => return Err(e),
+            }
+        }
+        println!();
+        Ok(response)
+    }
+
+    fn extract_code_block(&self, input: &str) -> anyhow::Result<String> {
+        let re = Regex::new(r"```(?:\w+)?\n?(?P<code>[\s\S]*?)\n?```")
+            .map_err(|e| anyhow!("Error creating regex: {}", e))?;
+
+        if let Some(captures) = re.captures(input) {
+            let code = captures
+                .name("code")
+                .ok_or_else(|| anyhow!("No code block found"))?;
+            Ok(code.as_str().to_string())
+        } else {
+            Err(anyhow!("No code block found"))
+        }
+    }
+
+    fn execute_command(&self, command: &str) -> anyhow::Result<String> {
+        let output = Command::new("sh").arg("-c").arg(&command).output()?;
+
+        if !output.status.success() {
+            let error_message = String::from_utf8_lossy(&output.stderr).to_string();
+            Err(anyhow::anyhow!(
+                "Command failed: {}\nError message: {}",
+                command,
+                error_message
+            ))
+        } else {
+            let success_message = String::from_utf8_lossy(&output.stdout).to_string();
+            Ok(success_message)
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
-mod openai;
-
-use anyhow::anyhow;
-use futures::{Stream, StreamExt};
-use openai::OpenAIWrapper;
-use regex::Regex;
+use okclai::{OkClai, OpenAIWrapper, Settings};
 use reqwest::Client;
-use std::pin::Pin;
-use std::process::Command;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -29,76 +22,13 @@ async fn main() -> anyhow::Result<()> {
     let client = Client::new();
     let openai_api_key = get_api_key()?;
     let open_ai_wrapper = OpenAIWrapper::new(&openai_api_key, &client);
+    let settings = Settings::new(!opt.no_stream);
+    let okclai = OkClai::new(open_ai_wrapper, settings);
 
     let command_descripton = opt.command_description.join(" ");
-
-    let response: anyhow::Result<String> = if opt.no_stream {
-        let response = open_ai_wrapper.get_response(&command_descripton).await?;
-        println!("{}", response);
-        Ok(response)
-    } else {
-        let response_stream = Box::pin(
-            open_ai_wrapper
-                .get_streaming_response(&command_descripton)
-                .await?,
-        );
-        let response = print_and_extract_response(response_stream).await?;
-        Ok(response)
-    };
-
-    let command = extract_code_block(&response?)?;
-    println!("\nCommand to execute: {:?}", command);
-    let result = execute_command(&command)?;
-    print!("\nOutput:\n{}", result);
+    okclai.execute(&command_descripton).await?;
 
     Ok(())
-}
-
-async fn print_and_extract_response(
-    mut stream: Pin<Box<impl Stream<Item = Result<String, anyhow::Error>>>>,
-) -> anyhow::Result<String> {
-    let mut response = String::new();
-    while let Some(result_token) = stream.next().await {
-        match result_token {
-            Ok(token) => {
-                print!("{}", token);
-                response = format!("{}{}", response, token);
-            }
-            Err(e) => return Err(e),
-        }
-    }
-    println!();
-    Ok(response)
-}
-
-fn extract_code_block(input: &str) -> anyhow::Result<String> {
-    let re = Regex::new(r"```(?:\w+)?\n?(?P<code>[\s\S]*?)\n?```")
-        .map_err(|e| anyhow!("Error creating regex: {}", e))?;
-
-    if let Some(captures) = re.captures(input) {
-        let code = captures
-            .name("code")
-            .ok_or_else(|| anyhow!("No code block found"))?;
-        Ok(code.as_str().to_string())
-    } else {
-        Err(anyhow!("No code block found"))
-    }
-}
-
-fn execute_command(command: &str) -> anyhow::Result<String> {
-    let output = Command::new("sh").arg("-c").arg(&command).output()?;
-
-    if !output.status.success() {
-        let error_message = String::from_utf8_lossy(&output.stderr).to_string();
-        Err(anyhow::anyhow!(
-            "Command failed: {}\nError message: {}",
-            command,
-            error_message
-        ))
-    } else {
-        let success_message = String::from_utf8_lossy(&output.stdout).to_string();
-        Ok(success_message)
-    }
 }
 
 fn get_api_key() -> anyhow::Result<String> {
